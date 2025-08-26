d5ecb402e1fe559e29e0b5755ca69c63929f432e
Manya Kalra <146841072+manyakalra05@users.noreply.github.com>
2025-08-26 22:56:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import Resources from './components/Resources';
+import DoubtCenter from "./components/DoubtCenter";
+import Connect from "./components/Connect";
 
 function App() {
   return (
     <Routes>
-
       <Route path="/" element={<LandingPage />} />
       <Route path="/resources" element={<Resources />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/doubt-center" element={<DoubtCenter />} />
+      <Route path="/connect" element={<Connect />} />
     </Routes>
   );
 }
